7ab7b872419d1daf23f99f4fff6aad492f2f15ad
Suresh Kumar <suresh.kumar@centricsource.com>
2016-08-17 22:47:54 +0500

    aded employer tem

---
+++ resources/views/frontend/employer/side_bar.blade.php
@@ -0,0 +1,44 @@
+<div class="admin-sidebar">
+										
+									<div class="admin-user-item for-employer">
+										
+										<div class="image">
+											<img src="images/brands/06.png" alt="image" class="img-circle" />
+										</div>
+										
+										<h4>Expedia</h4>
+										
+									</div>
+									
+									<div class="admin-user-action text-center">
+									
+										<a href="#" class="btn btn-primary btn-sm">Post Job</a>
+										<a href="#" class="btn btn-primary btn-sm btn-inverse">Create Resume</a>
+										
+									</div>
+									
+									<ul class="admin-user-menu clearfix">
+										<li>
+											<a href="#"><i class="fa fa-tachometer"></i> Dashboard</a>
+										</li>
+										<li>
+											<a href="#"><i class="fa fa-user"></i> Profile</a>
+										</li>
+										<li class="active">
+											<a href="#"><i class="fa fa-key"></i> Change Password</a>
+										</li>
+										<li>
+											<a href="#"><i class="fa fa-bell"></i> My Alert</a>
+										</li>
+										<li>
+											<a href="#"><i class="fa fa-bookmark"></i> Saved Jobs</a>
+										</li>
+										<li>
+											<a href="#"><i class="fa fa-pencil"></i> Resume Lists</a>
+										</li>
+										<li>
+											<a href="#"><i class="fa fa-sign-out"></i> Logout</a>
+										</li>
+									</ul>
+									
+								</div>
