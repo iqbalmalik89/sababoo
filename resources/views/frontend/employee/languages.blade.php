da6e9ebfbb746b021a6563dd7618b0fa59ec64c2
Iqbal Malik <iqbal_malik89@yahoo.com>
2016-08-19 18:24:38 +0500

    adding logo

--- resources/views/frontend/employee/languages.blade.php
+++ resources/views/frontend/employee/languages.blade.php
@@ -1,3 +1,4 @@
+<form id="language_form">
                                                 <div class="col-sm-12 col-md-12 mt-15">
                                                 
                                                     <h4 class="heading font700 mb-10 text-primary">Langauages</h4>
@@ -12,7 +13,13 @@
          
                                                 <div style="clear:both;">
 
+                                                    <div id="dynamicAddForm2_noforms_template" class="dynamic-add-form-empty">
+                                                        <div id="msg_language_form"></div>
+
+                                                    </div>
+
                                                 <div id="" class="dynamic-add-form-action">
                                                         <div id="dynamicAddForm4_add"><button onclick="addMoreLanguage(1, '', '');" class="btn btn-primary btn-sm"><span>Add Language</span></button></div> 
                                                         <div id="dynamicAddForm4_add"><button onclick="saveUserLanguages();" class="btn btn-primary btn-sm"><span>Save Language</span></button></div>                                                               
                                                 </div>
+</form>
