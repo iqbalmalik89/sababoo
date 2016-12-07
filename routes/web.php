c12b87b737ec3e9ca1e1e14a9d858938e7c8fa24
Suresh Kumar <skamrani2002@gmail.com>
2016-12-07 12:50:00 +0500

    aded

--- routes/web.php
+++ routes/web.php
@@ -108,7 +108,7 @@ Route::match(['get', 'post'], '/user/delete_user_file', ['uses' => 'UI\UserContr
 Route::match(['get', 'post'], '/user/download_files/{file_id}', ['uses' => 'UI\UserController@DownloadFiles']);
 
 /************************************JOB POSTING *****************************************************/
-Route::match(['get', 'post'], '/job/post', ['uses' => 'UI\JobPostController@jobPost'])->middleware(['acl.front:job.create']);
+Route::match(['get', 'post'], '/job/post', ['uses' => 'UI\JobPostController@jobPost'])->middleware(['acl.front']);
 Route::match(['get', 'post'], '/job/job_create', ['uses' => 'UI\JobPostController@jobCreate'])->middleware(['acl.front:job.create']);
 Route::match(['get', 'post'], '/job/user_job_list', ['uses' => 'UI\JobPostController@userJobList'])->middleware(['acl.front:job.list']);
 Route::match(['get', 'post'], '/job/job_delete', ['uses' => 'UI\JobPostController@delJob'])->middleware(['acl.front:job.delete']);
